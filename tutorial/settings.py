37d86384a15eb0beeb3bc0d9e4231b2ba5d71f17
Andrzej Kostański <andrzej.kostanski@adpilot.pl>
2016-12-06 18:12:06 +0100

    2_DRF_Tutorial Requests and Responses from DRF and format suffixes for renderers

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'PAGE_SIZE': 10
 }
 
