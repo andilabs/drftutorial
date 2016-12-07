440b896871d5d26e65c1332074b8c69f8bc43fd2
Andrzej Kostański <andrzej.kostanski@adpilot.pl>
2016-12-07 17:23:39 +0100

    4_DRF_Tutorial Authentication and Permissions

--- snippets/views.py
+++ snippets/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-from rest_framework import mixins
+from rest_framework import permissions
+
+from permissions import IsOwnerOrReadOnly
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
@@ -13,6 +15,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -21,3 +27,5 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -7,15 +7,12 @@ from tutorial.quickstart import views
 
 admin.autodiscover()
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include('snippets.urls')),
-    url(r'^', include(router.urls)),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 ]
