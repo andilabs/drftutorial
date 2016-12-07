8cc6a816ea2ed811cfbd325ab36570ff2329d26e
Andrzej Kostański <andrzej.kostanski@adpilot.pl>
2016-12-07 10:46:05 +0100

    3_DRF_Tutorial version of CBV using DRF generic CBV

--- snippets/views.py
+++ snippets/views.py
@@ -7,42 +7,17 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        # list comes from mixins.ListModelMixin
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        # create comes from mixins.CreateModelMixin
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a snippet instance.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        # retrieve comes from mixins.RetrieveModelMixin
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        # update comes from  mixins.UpdateModelMixin
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        # delete comes from mixins.DestroyModelMixin
-        return self.destroy(request, *args, **kwargs)
