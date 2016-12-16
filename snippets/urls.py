9fe55cd3e9437f6b0e8d52a92a0887342937e966
Andrzej Kostański <andrzej.kostanski@adpilot.pl>
2016-12-16 17:21:16 +0100

    7_DRF_Tutorial added support for coreapi and schemas in general

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,20 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.schemas import get_schema_view
 from snippets.views import SnippetViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'snippets', SnippetViewSet)
 router.register(r'users', UserViewSet)
+schema_view = get_schema_view(title='Pastebin API')
+
+# READING on SCHEMA:
+# http://www.django-rest-framework.org/api-guide/schemas/
+# http://www.coreapi.org/specification/encoding/#core-json-encoding
+# https://www.openapis.org/
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
